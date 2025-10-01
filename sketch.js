5eab75f17b261aec655bcbfef2db0f8dd55ceb3a
Réal DL <reald@ocadu.ca>
2025-10-01 16:31:43 -0400

    Getting started

---
+++ sketch.js
@@ -0,0 +1,17 @@
+let flower;
+
+function preload()
+{
+	flower = loadImage("flower.gif");
+}
+
+function setup() 
+{
+	createCanvas(400, 400);
+}
+
+function draw()
+{
+!background(255,0,255);
+//image(flower,-100,-100);
+}
