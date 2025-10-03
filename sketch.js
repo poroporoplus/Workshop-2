5db20d15a17ee7871c401acd7a60cd38e6017769
Réal DL <reald@ocadu.ca>
2025-10-03 16:05:35 -0400

    2nd test

--- sketch.js
+++ sketch.js
@@ -57,7 +57,7 @@ function draw()
     // Check if sensors are working
     if (window.sensorsEnabled) 
     {
-        background(orientationX, orientationY, orientationZ);  // Light green when sensors active
+        background(rotationX, rotationY, rotationZ);  // Light green when sensors active
         
         // Update orientation values from device sensors ONLY when enabled
         orientationX = rotationX;
