0ed029902865c7239f01a99d48c0ffee29f8496b
Réal DL <reald@ocadu.ca>
2025-10-13 14:13:46 -0400

    overhaul

--- sketch.js
+++ sketch.js
@@ -1,67 +1,65 @@
-
-let orientationX = 0; 
+let orientationX = 0;
 let orientationY = 0;  
 let orientationZ = 0;  
 
 function preload() {
-    gif = loadImage('rainbow.gif');
+    rainbow = loadImage('rainbow.gif');
 }
 
-function setup() 
+function setup()
 {
 
     createCanvas(windowWidth, windowHeight);
 
     angleMode(DEGREES);
-    
+   
     lockGestures();
-    
+   
     enableGyroTap('ENABLE MOTION SENSORS', 'Requesting motion sensors...');
-    
+   
     textAlign(CENTER, CENTER);
     textSize(32);
-    
+   
 }
 
-function draw() 
+function draw()
 {
     background(240, 240, 240);
-    
-    if (window.sensorsEnabled) 
+   
+    if (window.sensorsEnabled)
     {
-        
+       
         orientationX = rotationX;
         orientationY = rotationY;
         orientationZ = rotationZ;
 
         debug('angles',orientationX,orientationY,orientationZ);
 
-		background(orientationX, orientationY, orientationZ); 
-        
-        image(gif, width/2, height/2, 150, 100);
+        background(orientationX, orientationY, orientationZ);  
 
+        image(rainbow, width/2, height/2, 150, 100);
 
-    } 
-    else 
+    }
+    else
     {
         fill(50, 50, 50);  
         textAlign(CENTER, CENTER);
         textSize(28);
         fill(150, 50, 50);  
         text("Motion Sensors Disabled", width/2, height/2 - 40);
-        
+       
         textSize(20);
         fill(100, 100, 100);
         text("Tap the screen to enable\\nmotion sensors", width/2, height/2 + 20);
     }
 }
 
-function touchStarted() 
+function touchStarted()
 {
     return false;
 }
 
-function touchEnded() 
+function touchEnded()
 {
     return false;
 }
